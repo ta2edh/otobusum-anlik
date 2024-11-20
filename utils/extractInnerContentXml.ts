f0a0a636bd3ee801529e63d0977df1178ebd5801
metkm <metinkorkmaz417@gmail.com>
2024-11-20 17:34:48 +0300

    change extractInnerContentXml function with arrow function

--- utils/extractInnerContentXml.ts
+++ utils/extractInnerContentXml.ts
@@ -1,3 +1,3 @@
-export function extractInnerContentXml(key: string, content: string) {
+export const extractInnerContentXml = (key: string, content: string) => {
   return content.split(`${key}>`).at(1)?.split(`</`).at(0)
 }
