35e8e694dd9d4a77556cc3cf041171c073b0f7c6
metkm <metinkorkmaz417@gmail.com>
2024-12-02 22:46:24 +0300

    isStop function to arrow function

--- utils/isStop.ts
+++ utils/isStop.ts
@@ -1,5 +1,5 @@
 import { BusLine, BusStop } from '@/types/bus'
 
-export function isStop(item: BusStop | BusLine): item is BusStop {
+export const isStop = (item: BusStop | BusLine): item is BusStop => {
   return (item as BusStop).stop_code !== undefined
 }
