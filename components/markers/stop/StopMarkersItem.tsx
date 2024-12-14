087bc0075128fc2368190380dc6dda4c9ebdbb45
metkm <metinkorkmaz417@gmail.com>
2024-12-14 21:49:04 +0300

    fix label color on clustered stops

--- components/markers/stop/StopMarkersItem.tsx
+++ components/markers/stop/StopMarkersItem.tsx
@@ -65,7 +65,7 @@ export const LineBusStopMarkersItem = ({
 
   const textStyle: StyleProp<TextStyle> = useMemo(
     () => ({
-      color: getSchemeColorHex('onPrimary'),
+      color: getSchemeColorHex('primary'),
     }),
     [getSchemeColorHex],
   )
