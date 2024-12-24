889c2161e735e84c473519f2f2a9bb03779af9a8
metkm <metinkorkmaz417@gmail.com>
2024-12-24 23:40:52 +0300

    read base url from env variable

--- api/api.ts
+++ api/api.ts
@@ -2,14 +2,12 @@ import ky, { Options } from 'ky'
 
 import { useFiltersStore } from '@/stores/filters'
 
-const baseUrl = 'https://otobusum.metkm.win'
-
 export async function api<T>(url: string, options?: Options): Promise<T> {
   const selectedCity = useFiltersStore.getState().selectedCity
 
   const params = (options?.searchParams || {}) as Record<string, any>
 
-  const response = await ky.get<T>(`${baseUrl}${url}`, {
+  const response = await ky.get<T>(`${process.env.BASE_URL}${url}`, {
     ...options,
     headers: {
       ...options?.headers,
