38e28c967fa3311c81810cc654733ca1adce6775
metkm <metinkorkmaz417@gmail.com>
2025-01-08 19:40:53 +0300

    move callout

--- components/markers/bus/BusMarkersItem.tsx
+++ components/markers/bus/BusMarkersItem.tsx
@@ -1,20 +1,18 @@
 import Ionicons from '@react-native-vector-icons/ionicons'
 import { memo } from 'react'
-import { StyleProp, StyleSheet, TextStyle, View, ViewStyle } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { MapMarkerProps } from 'react-native-maps'
 import { useShallow } from 'zustand/react/shallow'
 
-import { UiText } from '@/components/ui/UiText'
-
-import { useLine } from '@/hooks/queries/useLine'
 import { useTheme } from '@/hooks/useTheme'
 
 import { MarkerLazyCallout } from '../MarkerLazyCallout'
 
+import { BusMarkersCallout } from './BusMarkersCallout'
+
 import { BusLocation } from '@/api/getLineBusLocations'
 import { colors } from '@/constants/colors'
 import { getTheme, useLinesStore } from '@/stores/lines'
-import { i18n } from '@/translations/i18n'
 
 interface LineBusMarkersItemProps extends Omit<MapMarkerProps, 'coordinate'> {
   location: BusLocation
@@ -24,43 +22,14 @@ interface LineBusMarkersItemProps extends Omit<MapMarkerProps, 'coordinate'> {
 export const LineBusMarkersItem = ({ location, lineCode }: LineBusMarkersItemProps) => {
   const lineTheme = useLinesStore(useShallow(() => getTheme(lineCode)))
   const { getSchemeColorHex } = useTheme(lineTheme)
-  const { query: { dataUpdatedAt } } = useLine(lineCode)
 
   const textColor = getSchemeColorHex('onPrimaryContainer')
   const backgroundColor = getSchemeColorHex('primaryContainer')
 
-  const dynamicCalloutContainer: StyleProp<ViewStyle> = {
-    backgroundColor: getSchemeColorHex('primary'),
-  }
-
-  const textStyle: StyleProp<TextStyle> = {
-    color: getSchemeColorHex('onPrimary'),
-    fontWeight: 'bold',
-  }
-
   return (
     <MarkerLazyCallout
       calloutProps={{
-        children: (
-          <View style={[styles.calloutContainer, dynamicCalloutContainer]}>
-            {location.route_code && (
-              <UiText style={textStyle}>
-                {location.route_code}
-              </UiText>
-            )}
-
-            <UiText style={textStyle}>
-              {i18n.t('doorNo')}
-              {': '}
-              {location.bus_id}
-            </UiText>
-            <UiText style={textStyle}>
-              {i18n.t('lastUpdate')}
-              {': '}
-              {new Date(dataUpdatedAt).toLocaleTimeString()}
-            </UiText>
-          </View>
-        ),
+        children: <BusMarkersCallout busLocation={location} lineCode={lineCode} />,
       }}
       markerProps={{
         coordinate: {
