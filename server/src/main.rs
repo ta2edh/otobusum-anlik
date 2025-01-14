39096b8035f7cbfde96586a939491ed077d85270
metkm <metinkorkmaz417@gmail.com>
2025-01-14 19:31:14 +0300

    update

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,7 +4,7 @@ use axum::{serve, Router};
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let router = Router::new()
-        .nest_service("/", ServeDir::new("../dist"));
+        .fallback_service(ServeDir::new("../dist"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
