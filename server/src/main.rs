944fd6ef24a71fe59a729acdf0ea3d80177112a7
metkm <metinkorkmaz417@gmail.com>
2025-01-14 18:42:09 +0300

    init

---
+++ server/src/main.rs
@@ -0,0 +1,16 @@
+use tower_http::services::ServeDir;
+use axum::{serve, Router};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let router = Router::new()
+        .nest_service("/", ServeDir::new("../dist"));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    serve(listener, router)
+        .await
+        .unwrap();
+}
