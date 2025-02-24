a37ca3b87ca286be4746fb8f19b65ddfbfb70daf
metkm <metinkorkmaz417@gmail.com>
2025-02-24 21:49:05 +0300

    use loop to render screens

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -6,10 +6,31 @@ import { useTheme } from '@/hooks/useTheme'
 
 import { i18n } from '@/translations/i18n'
 
+const screens = [
+  {
+    name: 'index',
+    label: 'map',
+    icon: 'map',
+  },
+  {
+    name: 'search',
+    label: 'search',
+    icon: 'search',
+  },
+  {
+    name: 'timetable',
+    label: 'timetable',
+    icon: 'time',
+  },
+  {
+    name: 'settings',
+    label: 'settings',
+    icon: 'settings',
+  },
+]
+
 export const TabsLayout = () => {
   const { colorsTheme } = useTheme()
-
-  const getName = (n: ComponentProps<typeof Ionicons>['name'], focused?: boolean): any => (focused ? `${n}` : `${n}-outline`)
 
   return (
     <Tabs
@@ -24,42 +45,26 @@ export const TabsLayout = () => {
       }}
       detachInactiveScreens
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarLabel: i18n.t('map'),
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name={getName('map', focused)} size={24} color={colorsTheme.color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarLabel: i18n.t('search'),
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name={getName('search', focused)} size={24} color={colorsTheme.color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="timetable"
-        options={{
-          tabBarLabel: i18n.t('timetable'),
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name={getName('time', focused)} size={24} color={colorsTheme.color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          tabBarLabel: i18n.t('settings'),
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name={getName('settings', focused)} size={24} color={colorsTheme.color} />
-          ),
-        }}
-      />
+      {screens.map(screen => (
+        <Tabs.Screen
+          key={screen.name}
+          name={screen.name}
+          options={{
+            tabBarLabel: i18n.t(screen.label),
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name={
+                  (focused ? `${screen.icon}` : `${screen.icon}-outline`) as ComponentProps<
+                    typeof Ionicons
+                  >['name']
+                }
+                size={24}
+                color={colorsTheme.color}
+              />
+            ),
+          }}
+        />
+      ))}
     </Tabs>
   )
 }
