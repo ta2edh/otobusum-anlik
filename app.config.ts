8d37700316953d4c616461f51d85181f480ecdba
metkm <metinkorkmaz417@gmail.com>
2025-05-06 18:51:11 +0300

    bump version

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { ExpoConfig } from 'expo/config'
 const config: ExpoConfig = {
   name: 'Otobüsüm Anlık',
   slug: 'otobusum-anlik',
-  version: '1.2.5',
+  version: '1.2.8',
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   scheme: 'otobusum-anlik',
