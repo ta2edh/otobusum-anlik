794af0818bd86ea213af119af8ef88e3ad3ef695
metkm <metinkorkmaz417@gmail.com>
2025-06-24 23:05:05 +0300

    increment version

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { ExpoConfig } from 'expo/config'
 const config: ExpoConfig = {
   name: 'Otobüsüm Anlık',
   slug: 'otobusum-anlik',
-  version: '1.2.8',
+  version: '1.2.9',
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   scheme: 'otobusum-anlik',
